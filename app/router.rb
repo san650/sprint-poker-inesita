b3e610cfbfb7cb681215736749e4e53ec42f2bb3
Michał Kalbarczyk <fazibear@gmail.com>
2017-04-11 12:38:34 +0200

    Welcome screen

--- app/router.rb
+++ app/router.rb
@@ -2,7 +2,7 @@ class Router
   include Inesita::Router
 
   def routes
-    route '/', redirect_to: :new_game_screen
+    route '/', to: WelcomeScreen
     route '/new-game', to: NewGameScreen
     route '/games/:game_id', to: PreGameScreen
   end
---
+++ app/screens/welcome_screen.rb
@@ -0,0 +1,7 @@
+class WelcomeScreen
+  include Inesita::Component
+
+  def render
+    h1 { "Hello!" }
+  end
+end
